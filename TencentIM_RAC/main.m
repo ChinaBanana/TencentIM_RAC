8e33c74978dc3310930f05835b600a4031a85308
ChinaBanana <zhw18840839565@icloud.com>
2017-02-12 17:46:31 +0800

    Initial Commit

---
+++ TencentIM_RAC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TencentIM_RAC
+//
+//  Created by 赵海伟 on 12/02/2017.
+//  Copyright © 2017 赵海伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
